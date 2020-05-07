4443bc1d9135d042d720b21cf32638a15ea751a1
Matt Langbehn <matthew.langbehn@gmail.com>
2020-05-07 14:42:54 -0500

    Make aggregate-labs.sh functional ;-)
    Also made it a bit more robust...more logic coming soon.
    
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- aggregate-labs.sh
+++ aggregate-labs.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
-rm -r tempClones
-mkdir tempClones
-rm -r workshop/generatedContent/*
-rm workshop/generatedContentLinks.md
+clean_tempClones(){
+    rm -rf tempClones
+}
 
-grep -v '^ *#' < agenda.txt | while IFS= read -r repo
-do
-    echo "adding $repo to agenda"
-    workshopName=$(basename "$repo")
-	git clone "$repo" tempClones/"$workshopName"
-	cp -a tempClones/"$workshopName"/workshop workshop/generatedContent/"$workshopName"
-	echo "* [generatedContent/$workshopName](generatedContent/$workshopName/README.md)" >> workshop/generatedContentLinks.md
-	for lab in workshop/generatedContent/"$workshopName"/*/*; do
-        [[ -d "$lab" ]] || break
-		labName=$(basename "$lab")
-		echo "    * [generatedContent/$workshopName/$labName](generatedContent/$workshopName/$labName/README.md)" >> workshop/generatedContentLinks.md
-		echo "Lab -> $(basename "$lab")"
-	done
-	echo "This content is generated! Do not edit directly! Please run aggregate-labs.sh to repopulate with latest content from agenda.txt!" > workshop/generatedContent/README.md
-done
+clean_generatedContent(){
+    rm -rf workshop/generatedContent/*
+    rm workshop/generatedContentLinks.md
+}
+
+do_the_thing(){
+    grep -v '^ *#' < agenda.txt | while IFS= read -r repo; do
+        echo "adding $repo to agenda"
+        workshopName=$(basename "$repo")
+    	git clone "$repo" tempClones/"$workshopName"
+    	cp -a tempClones/"$workshopName"/workshop workshop/generatedContent/"$workshopName"
+    	echo "* [generatedContent/$workshopName](generatedContent/$workshopName/README.md)" >> workshop/generatedContentLinks.md
+    	for lab in workshop/generatedContent/"$workshopName"/*/*; do
+            [[ -d "$lab" ]] || break
+    		labName=$(basename "$lab")
+    		echo "    * [generatedContent/$workshopName/$labName](generatedContent/$workshopName/$labName/README.md)" >> workshop/generatedContentLinks.md
+    		echo "Lab -> $(basename "$lab")"
+    	done
+    	echo "This content is generated! Do not edit directly! Please run aggregate-labs.sh to repopulate with latest content from agenda.txt!" > workshop/generatedContent/README.md
+    done
+}
+
+main(){
+    pushd "$(dirname "$0")" || exit 1
+    if [[ -d tempClones ]]; then
+        clean_tempClones
+     fi
+    mkdir -p tempClones workshop/generatedContent
+    clean_generatedContent
+    do_the_thing
+    clean_tempClones
+    popd || exit 1
+}
+
+main
